a822ee0f2ba780d05b1741c8fe369c8bcfc15db4
rustock0 <rustock@lna.org.ru>
2015-06-24 23:59:09 +0300

    #13; jviewer-console artifact has been added

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-unzip -u jviewer-build/target/jviewer-build-2.0-build.zip
-echo Installing JViewer-Info...
-mv jviewer-info-2.0.war /c/tomcat/webapps/info.war
-echo Installing Jviewer-Main...
-mv jviewer-main-2.0.war /c/tomcat/webapps/main.war
-rm -rf /c/tomcat/webapps/info
-rm -rf /c/tomcat/webapps/main
-echo Deploy was successful!
--- jviewer-refresh-lib/src/main/java/tk/jviewer/refresh/RefreshServiceImpl.java
+++ jviewer-refresh-lib/src/main/java/tk/jviewer/refresh/RefreshServiceImpl.java
@@ -1,20 +1,17 @@
 package tk.jviewer.refresh;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.Map;
 
 /**
  * Implementation of {@link RefreshServiceImpl}. Can be invoked from JMX console.
  * @author Evgeny Mironenko
  */
-@ManagedResource(objectName = "jviewer:bean=tk.jviewer.refresh")
+@ManagedResource(objectName = "jviewer:bean=tk.jviewer.refresh.RefreshService")
 public class RefreshServiceImpl implements RefreshService {
 
     private ApplicationContext context;
