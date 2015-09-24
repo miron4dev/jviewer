069c4424efde621c79c3777bbd272174f6bd9aa8
rustock0 <rustock@lna.org.ru>
2015-09-25 00:33:08 +0300

    #9; JViewer-Info optimization

--- jviewer-info/src/main/java/tk/jviewer/info/model/News.java
+++ jviewer-info/src/main/java/tk/jviewer/info/model/News.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
  */
 public class News implements Serializable {
 
+    private static final long serialVersionUID = 2491387418422869005L;
+
     private Integer id;
     private String topic;
     private String text;
--- jviewer-info/src/main/java/tk/jviewer/info/model/UserProfile.java
+++ jviewer-info/src/main/java/tk/jviewer/info/model/UserProfile.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
  */
 public class UserProfile implements Serializable {
 
+    private static final long serialVersionUID = 3769255827201866140L;
+
     private String name;
     private String password;
 
