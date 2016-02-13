07f432e8ae79cb92eb0fcf31be0fc0f1e409d3e9
rustock0 <rustock@lna.org.ru>
2016-02-13 20:18:37 +0300

    #39; ViewerManagedBean has been defined via @Component

--- jviewer-web/src/main/java/tk/jviewer/model/ViewerManagedBean.java
+++ jviewer-web/src/main/java/tk/jviewer/model/ViewerManagedBean.java
@@ -1,10 +1,15 @@
 package tk.jviewer.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 import tk.jviewer.business.model.RoomEntity;
 
 /**
  * Contains state of the Viewer dialog.
  */
+@Component
+@Scope(scopeName = WebApplicationContext.SCOPE_SESSION)
 public class ViewerManagedBean {
 
     private RoomEntity currentRoom;
