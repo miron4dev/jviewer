c3b9e8722f1790dac861f26b8dc45d3b4e10d1e7
rustock0 <rustock@lna.org.ru>
2016-02-13 20:44:46 +0300

    Fixed #50; Feedback form has been implemented

---
+++ jviewer-web/src/main/java/tk/jviewer/tag/FeedBackTag.java
@@ -0,0 +1,66 @@
+package tk.jviewer.tag;
+
+import tk.jviewer.business.api.FeedbackService;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.mail.MessagingException;
+import java.io.Serializable;
+
+/**
+ * Feedback tag backing bean.
+ */
+public class FeedbackTag implements Serializable {
+
+    private static final long serialVersionUID = -4759174795962760617L;
+
+    private String name;
+    private String email;
+    private String text;
+
+    private FeedbackService feedbackService;
+
+    /**
+     * Submits the feedback.
+     */
+    public void submit() {
+        try {
+            feedbackService.sendFeedback(name, email, text);
+        } catch (MessagingException e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                "Could not send the message because of connection problem. Please contact with your system administrator.", null));
+        }
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //
+    // Dependency Injection
+    //
+
+    public void setFeedbackService(FeedbackService feedbackService) {
+        this.feedbackService = feedbackService;
+    }
+}
